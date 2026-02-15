cbc9b24427a38c5d71019d130041d1f8ea71187a
zkerkeb <zakaria.kerkeb@ynov.com>
2026-02-15 17:35:18 +0100

    refactor: remove authentication and pokemon routes, middleware, and schemas

--- index.js
+++ index.js
@@ -3,36 +3,23 @@
 import 'dotenv/config';
 
 import express from 'express';
-import pokemon from './schema/pokemon.js';
 import cors from 'cors';
-import authRoutes from './routes/auth.js';
-import authMiddleware from './middleware/auth.js';
-import pokemonRoutes from './routes/pokemons.js';
 
 
 import './connect.js';
 
 const app = express();
 
-app.use(cors({
-    origin: 'http://localhost:5173',
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-}));
+app.use(cors()); // Permet les requêtes cross-origin (ex: frontend sur un autre port)
 
-app.use('/assets', express.static('assets'));
+app.use('/assets', express.static('assets')); // Permet d'accéder aux fichiers dans le dossier "assets" via l'URL /assets/...
 
 app.use(express.json());
 
-app.use((req, res, next) => {
-console.log(` envoi de stats ${req.method} ${req.url}`); next(); }); // === Routes d'authentification (publiques) === // Toutes les routes définies dans routes/auth.js seront préfixées par /auth // Exemple : POST /auth/register, POST /auth/login, GET /auth/me app.use('/auth', authRoutes);
 
 app.get('/', (req, res) => {
     res.send('Hello, World!');
 });
-
-
-app.use('/auth', authRoutes);
-app.use('/pokemons', pokemonRoutes);
 
 
 
